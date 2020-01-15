cb6ed2289858b42e77d311f1a12213336d832c1f
Abid Talukdar <abiditalukdar@gmail.com>
2020-01-15 22:57:01 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -82,7 +82,7 @@ def gross_per_studio(collection)
   gross_per_studio = {}
   index_num = 0 
   while index_num < collection.length do 
-    
+    gross_per_studio += collection[index_num]
   index_num+=1
   end
   
