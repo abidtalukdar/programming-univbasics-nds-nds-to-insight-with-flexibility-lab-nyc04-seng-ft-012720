f8e01f8cf1441b8fa9cc678db560cbf3ea9e2c48
Abid Talukdar <abiditalukdar@gmail.com>
2020-01-15 22:46:58 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -90,22 +90,30 @@ def gross_per_studio(collection)
   
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
 end
 
 
+def gross_for_director(director_data)
+  director_movies = director_data[:movies]
+  total = 0
+  column_index = 0
+  while column_index < director_movies.length do
+    total += director_movies[column_index][:worldwide_gross]
+    column_index += 1
+  end  
+  return total
+end
 
-
-
-
+def directors_totals(nds)
+  result = {}
+  row_index = 0
+  while row_index < nds.length do
+    result[nds[row_index][:name]] = gross_for_director(nds[row_index])
+    row_index += 1
+  end
+  return result
+end
 
 
 
